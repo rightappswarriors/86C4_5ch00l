3d20432f9a10f1b428bc1f971d168ece6a6dd9d8
Eboty16 <rai.velezvincent@gmail.com>
2026-03-21 10:13:08 +0800

    Bug fixes on my profile model

--- application/models/Profile_model.php
+++ application/models/Profile_model.php
@@ -4,8 +4,8 @@ class Profile_model extends CI_Model
 	function getInfo()
 	{
 		$current_userid = $this->session->userdata('current_userid');
-		$this->db->select('register.*, register.lrn AS lrn');
-		$this->db->select("COALESCE(NULLIF(register.school_id, ''), register.school_id) AS school_id", FALSE);
+		$this->db->select('register.*');
+		//$this->db->select("COALESCE(NULLIF(register.school_id, ''), register.school_id) AS school_id", FALSE);
 		$this->db->from('register');
 		$this->db->where('register.id', $current_userid);
 
